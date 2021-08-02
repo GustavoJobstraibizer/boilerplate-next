117d69c356ad91b9c4f2cee72f66647766b2c044
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-02 09:37:30 -0300

    chore: removing unused variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const test = 'teste'
-const test2 = 'teste2'
-
 export default function Home() {
   return (
     <div className={styles.container}>
